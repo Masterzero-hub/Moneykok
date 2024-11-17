57f1b5c0e6fdd4eb8e5acd4ddf3ddf0d295c6f4b
chj <shine8648@naver.com>
2024-11-17 19:01:20 +0900

    feat: 초기 설계

---
+++ front/MoneyKok/src/components/common/HeaderNavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <RouterLink :to="{ name : 'home'}" class="navbar-brand">
+                <img src="@/assets/logo.png" width="100" alt="logo_img">
+            </RouterLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <RouterLink :to="{ name : 'deposits'}">예금 상품</RouterLink>
+                    <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+                </li>
+                <li class="nav-item">
+                    <RouterLink :to="{ name : 'savings'}">적금 상품</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink :to="{ name : 'ai'}">AI 상품 추천</RouterLink>                    
+                </li>
+                <li class="nav-item">
+                    <RouterLink :to="{ name : 'community' }">커뮤니티</RouterLink>                    
+                </li>
+                <li class="nav-item">
+                    <RouterLink :to="{ name : 'community' }">은행 찾기</RouterLink>                    
+                </li>
+                <button></button>
+            </ul>
+            </div>
+        </div>
+        </nav> 
+    </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/MoneyKok/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h1>홈</h1>
+  </main>
+</template>
+
+
+<script setup>
+  
+</script>
