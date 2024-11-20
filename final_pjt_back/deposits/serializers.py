1b0c7d3dd2843ecc8d1a12532c6a479fc33904c5
장인영 <dlsdud0704@naver.com>
2024-11-20 15:45:16 +0900

    feat : 금융상품 저장

--- final_pjt_back/deposits/serializers.py
+++ final_pjt_back/deposits/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
-from .models import DepositOptions, DepositProducts
+from .models import DepositOptions, DepositProducts, Banks
 
 class DepositProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepositProducts
         fields ="__all__"
-
+        read_only_fields = ('bank',)
 
 class DepositOptionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepositOptions
         fields ="__all__"
         read_only_fields = ('product',)
+
+class BanksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Banks
+        fields ="__all__"
--- final_pjt_back/deposits/urls.py
+++ final_pjt_back/deposits/urls.py
@@ -5,4 +5,5 @@ app_name = 'deposits'
 
 urlpatterns = [
     path('save_products/', views.save_products, name="save_products"),
+
 ]
