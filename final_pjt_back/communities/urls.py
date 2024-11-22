681f81109762aaffdc554042485be864816dac66
장인영 <dlsdud0704@naver.com>
2024-11-22 13:19:53 +0900

    feat: 게시글 작성, 수정, 삭제

--- final_pjt_back/communities/urls.py
+++ final_pjt_back/communities/urls.py
@@ -5,6 +5,8 @@ app_name = 'communities'
 
 urlpatterns = [
     path('', views.communities, name='communities'),
-    path('<int:pk>/', views.detail, name='detail'),
-
+    path('<int:article_pk>/', views.detail_update_delete, name='detail_update_delete'),
+    path('<int:article_pk>/like/', views.like_article, name='like_article'),
+    path('<int:article_pk>/comments/', views.comment_create, name='comment_create'),
+    path('<int:article_pk>/comments/<int:comment_pk>/', views.comment_update_delete, name='comment_update_delete'),
 ]
